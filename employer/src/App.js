e8d9da2bb599570d7ffe68feee5f84d6c1834d3b
ilyassel <ilyasselharak@gmail.com>
2022-12-08 16:04:42 +0100

    create file

---
+++ employer/src/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+import data from "./Data"
+function App() {
+  return (
+    <div>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ employer/src/Data.js
@@ -0,0 +1,87 @@
+let data = [
+    {
+        "id":0,
+        "name": "Liam",
+        "speciality": "frontend"
+    },
+    {
+        "id":1,
+        "name": "Parsa",
+        "speciality": "backend"
+    },
+    {
+        "id":2,
+        "name": "Noah",
+        "speciality": "frontend"
+    },
+    {
+        "id":3,
+        "name": "Lala",
+        "speciality": "frontend"
+    },
+    {
+        "id":4,
+        "name": "Mia",
+        "speciality": "backend"
+    },
+    {
+        "id":5,
+        "name": "Yasir",
+        "speciality": "backend"
+    },
+    {
+        "id":6,
+        "name": "Evelyn",
+        "speciality": "frontend"
+    },
+    {
+        "id":7,
+        "name": "Emma",
+        "speciality": "backend"
+    },
+    {
+        "id":8,
+        "name": "Sahar",
+        "speciality": "frontend"
+    },
+    {
+        "id":9,
+        "name": "Neo",
+        "speciality": "backend"
+    },
+    {
+        "id":10,
+        "name": "Damali",
+        "speciality": "frontend"
+    },
+    {
+        "id":11,
+        "name": "Olajuwon",
+        "speciality": "backend"
+    },
+    {
+        "id":12,
+        "name": "Elinda",
+        "speciality": "frontend"
+    },
+    {
+        "id":13,
+        "name": "Emoni",
+        "speciality": "frontend"
+    },
+    {
+        "id":14,
+        "name": "Olivia",
+        "speciality": "backend"
+    },
+    {
+        "id":15,
+        "name": "kheri",
+        "speciality": "frontend"
+    },
+    {
+        "id":16,
+        "name": "Isabella",
+        "speciality": "backend"
+    },
+]
